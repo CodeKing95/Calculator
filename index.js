1f61539cc9c3a90618066270c3f050cf7e795ee9
Nadir Ahmed <102181168+CodeKing95@users.noreply.github.com>
2024-10-15 12:02:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//Taschenrechner Projekt
+
+const display = document.getElementById("display");
+
+function appendToZeigen(input) {
+  display.value += input;
+}
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
